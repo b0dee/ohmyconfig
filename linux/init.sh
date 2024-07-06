35e9f9e861ceba4d5a4665880c6973cae007c6c3
n0bodee <thath@looksnice.com>
2024-07-06 17:27:44 +0100

    Added SSH key generation (github)

--- linux/init.sh
+++ linux/init.sh
@@ -10,23 +10,29 @@ apps="vim \
 
 # Setup
 
-echo Symlinking config files
+echo [+] Symlinking config files
 
-echo Updating and Upgrading apt repositories
+echo [+] Updating and Upgrading apt repositories
 sudo apt update -y  && sudo apt dist-upgrade -y 
 
-echo Installing Apps: ${apps}
+echo [+] Installing Apps: ${apps}
 sudo apt install ${apps}
-echo Installing Homebrew
+
+echo [+] Installing Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo Setting Git username
+echo [+] Setting Git username
 git config --global user.name "Joe Paterson"
 
-echo Setting Git email
+echo [+] Setting Git email
 git config --global user.email "1@1.com"
 
-echo Setting Git to automatically create remotes to track when pushing a branch
+echo [+] Setting Git to automatically create remotes to track when pushing a branch
 git config --global -add --bool push.autoSetupRemote true
 
-echo Creating SSH keys
+echo [+] Generating SSH keys
+ssh-keygen -q -N "" -t ed25519 -f ~/.ssh/id_ed25519-github
+echo [+] Created GitHub SSH key
+echo [+] Public key data:
+cat ~/.ssh/id_ed25519-github
+
