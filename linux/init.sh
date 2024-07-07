28d0c646bafae715e27d8fb5e090293632fd8e96
n0bodee <thath@looksnice.com>
2024-07-08 00:12:51 +0100

    Made script functional

--- linux/init.sh
+++ linux/init.sh
@@ -1,16 +1,19 @@
+#!/bin/bash
+
 # Variables
 apps="vim \
-     git  \
-     tmux \
-     curl \
-     wget \
-     nmap \
-     zsh  \
+      git  \
+      tmux \
+      curl \
+      wget \
+      nmap \
+      zsh  \
 "
 
 # Setup
-
 echo [+] Symlinking config files
+OHMY_DIR=$( dirname $(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ))
+find $OHMY_DIR/common $OHMY_DIR/linux -exec ln -s {} ~/ \;
 
 echo [+] Updating and Upgrading apt repositories
 sudo apt update -y  && sudo apt dist-upgrade -y 
@@ -21,18 +24,9 @@ sudo apt install ${apps}
 echo [+] Installing Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo [+] Setting Git username
-git config --global user.name "Joe Paterson"
-
-echo [+] Setting Git email
-git config --global user.email "1@1.com"
-
-echo [+] Setting Git to automatically create remotes to track when pushing a branch
-git config --global -add --bool push.autoSetupRemote true
-
 echo [+] Generating SSH keys
 ssh-keygen -q -N "" -t ed25519 -f ~/.ssh/id_ed25519-github
-echo [+] Created GitHub SSH key
+
 echo [+] Public key data:
 cat ~/.ssh/id_ed25519-github
 
