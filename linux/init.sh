3d4edc3e60df8b92fc7c9133db49311c8bd80a53
n0bodee <thath@looksnice.com>
2024-07-08 18:46:15 +0100

    Auto clone repositories

--- linux/init.sh
+++ linux/init.sh
@@ -30,3 +30,12 @@ ssh-keygen -q -N "" -t ed25519 -f ~/.ssh/id_ed25519-github
 echo [+] Public key data:
 cat ~/.ssh/id_ed25519-github
 
+read -p "Copy the above public key into your GitHub SSH keys and press any key to continue... " -n1 -s
+
+[[ -d ~/repos ]] || echo [+] Creating repo folder && mkdir ~/repos
+
+[[ -d ~/repos/ohmyconfig ]] || echo [+] Cloning b0dee/ohmyconfig && git clone git@github.com:b0dee/ohmyconfig.git ~/repos/ohmyconfig
+
+[[ -d ~/repos/vimwiki ]] || echo [+] Cloning b0dee/vimwiki && git clone git@github.com:b0dee/vimwiki.git ~/repos/vimiki 
+
+[[ -d ~/vimwiki ]] || echo [+] Creating symlink in $HOME to ~/repos/vimwiki && ln -s ~/repos/vimwiki ~/vimwiki
