d33e25f87c06d25a27c9b57884d46ab9ea5a32b6
n0bodee <thath@looksnice.com>
2024-07-12 21:31:41 +0100

    hardcoded paths?

--- test/run.sh
+++ test/run.sh
@@ -6,4 +6,4 @@ set -p
 cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
 
 : "${VADER_TEST_VIM:=vim}"
-eval "$VADER_TEST_VIM -Nu vimrc -c '+Vader! bujo.vader'" && echo Success || echo Failure
+eval "$VADER_TEST_VIM -Nu ~/repos/ohmyconfig/test/vimrc -c '+Vader! ~/repos/ohmyconfig/test/bujo.vader'" && echo Success || echo Failure
